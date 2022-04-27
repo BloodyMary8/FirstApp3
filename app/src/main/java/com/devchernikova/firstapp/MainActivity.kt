fde00f93ee6bdb0886e8ab5905141baba655b760
BloodyMary8 <masharabota1996@gmail.com>
2022-04-27 17:50:52 +0700

    without toast

--- app/src/main/java/com/devchernikova/firstapp/MainActivity.kt
+++ app/src/main/java/com/devchernikova/firstapp/MainActivity.kt
@@ -3,39 +3,13 @@ package com.devchernikova.firstapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle;
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        topAppBar.setOnMenuItemClickListener {
-            when (it.itemId) {
-                R.id.settings -> {
-                    Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
-                    true
-                }
-                else -> false
-            }
-        }
     }
-    bottom.setOnNavigationItemSelectedListener {
-
-        when (it.itemId) {
-            R.id.favorites -> {
-                Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
-                true
-            }
-            R.id.watch_later -> {
-                Toast.makeText(this, "Посмотреть похже", Toast.LENGTH_SHORT).show()
-                true
-            }
-            R.id.selections -> {
-                Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
-                true
-            }
-            else -> false
-        }
-    }
-    }
+}
