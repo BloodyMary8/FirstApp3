9aae424eb7b2e3aca33d63c5f12a7063d47db1a1
BloodyMary8 <masharabota1996@gmail.com>
2022-05-03 22:05:07 +0700

    Lesson 20, cardview, toast, viewbinding topbar

--- app/src/main/java/com/devchernikova/firstapp/MainActivity.kt
+++ app/src/main/java/com/devchernikova/firstapp/MainActivity.kt
@@ -1,15 +1,52 @@
 package com.devchernikova.firstapp
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle;
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
 import android.widget.Toast
-
+import androidx.appcompat.app.AppCompatActivity
+import com.devchernikova.firstapp.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.topAppBar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.settings -> {
+                    Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
+        binding.bottom.setOnNavigationItemSelectedListener {
+
+            when (it.itemId) {
+                R.id.favorites -> {
+                    Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.watch_later -> {
+                    Toast.makeText(this, "Посмотреть похже", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.selections -> {
+                    Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
+
+
+
+
