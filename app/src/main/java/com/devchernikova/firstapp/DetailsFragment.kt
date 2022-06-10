e83ae68e63cd288c33afac3c394f5629c7934ff6
BloodyMary8 <masharabota1996@gmail.com>
2022-06-10 15:22:55 +0700

    favorites film, search view

--- app/src/main/java/com/devchernikova/firstapp/DetailsFragment.kt
+++ app/src/main/java/com/devchernikova/firstapp/DetailsFragment.kt
@@ -1,28 +1,61 @@
 package com.devchernikova.firstapp
-
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.devchernikova.firstapp.R.layout.fragment_details
+import com.devchernikova.firstapp.databinding.FilmItemBinding
 import com.devchernikova.firstapp.databinding.FragmentDetailsBinding
 
 
-class DetailsFragment : Fragment(R.layout.fragment_details) {
+class DetailsFragment : Fragment(){
+
+    private lateinit var binding: FragmentDetailsBinding
+    private lateinit var film: Film
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_details, container, false)
+        return inflater.inflate(fragment_details, container, false)
     }
-    private lateinit var binding: FragmentDetailsBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentDetailsBinding.bind(view)
+
         setFilmsDetails()
+
+
+        binding.detailsFabFavorites.setOnClickListener {
+            val film = arguments?.get("film") as Film
+            if (!film.isInFavorites) {
+                binding.detailsFabFavorites.setImageResource(R.drawable.ic_baseline_favorite_24)
+                film.isInFavorites = true
+            } else {
+                binding.detailsFabFavorites.setImageResource(R.drawable.ic_baseline_favorite_border_24)
+                film.isInFavorites = false
+            }
+        }
+
+        binding.detailsFabShare.setOnClickListener {
+            val film = arguments?.get("film") as Film
+            //Создаем интент
+            val intent = Intent()
+            //Указываем action с которым он запускается
+            intent.action = Intent.ACTION_SEND
+            //Кладем данные о нашем фильме
+            intent.putExtra(
+                Intent.EXTRA_TEXT,
+                "Check out this film: ${film.title} \n\n ${film.description}"
+            )
+            //Указываем MIME тип, чтобы система знала, какое приложения предложить
+            intent.type = "text/plain"
+            //Запускаем наше активити
+            startActivity(Intent.createChooser(intent, "Share To:"))
+        }
     }
 
     private fun setFilmsDetails() {
@@ -35,6 +68,10 @@ class DetailsFragment : Fragment(R.layout.fragment_details) {
         binding.detailsPoster.setImageResource(film.poster)
         //Устанавливаем описание
         binding.detailsDescription.text = film.description
+        binding.detailsFabFavorites.setImageResource(
+            if (film.isInFavorites) R.drawable.ic_baseline_favorite_24
+            else R.drawable.ic_baseline_favorite_border_24
+        )
     }
 }
 
