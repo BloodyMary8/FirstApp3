37c00eeb8517e0b2b8edfb9b61171e1c806d9a31
BloodyMary8 <masharabota1996@gmail.com>
2022-10-27 13:26:25 +0700

    Broadcast receiver

--- app/src/main/java/com/devchernikova/firstapp/ConnectionChecker.kt
+++ app/src/main/java/com/devchernikova/firstapp/ConnectionChecker.kt
@@ -6,21 +6,3 @@ import android.content.Intent
 import android.widget.Toast
 
 
-class ConnectionChecker : BroadcastReceiver() {
-
-    override fun onReceive(context: Context?, intent: Intent?) {
-        //Если интента нет, то выходим из метода
-        if (intent == null) return
-        //Проверяем, какой пришел action
-        when (intent.action) {
-            //Если пришел низкий заряд батарее
-            Intent.ACTION_BATTERY_LOW -> Toast.makeText(context,
-                "Батарея разряжена",
-                Toast.LENGTH_SHORT).show()
-            //Если пришло подключение к зарядке
-            Intent.ACTION_POWER_CONNECTED -> Toast.makeText(context,
-                "Зарядка подключена",
-                Toast.LENGTH_SHORT).show()
-        }
-    }
-}
