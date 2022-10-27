6c5351c7dcc29f4fbd206c2e6d295b053f0787db
BloodyMary8 <masharabota1996@gmail.com>
2022-10-27 13:24:55 +0700

    Broadcast receiver

--- app/src/main/java/com/devchernikova/firstapp/MainActivity.kt
+++ app/src/main/java/com/devchernikova/firstapp/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.devchernikova.firstapp
 
 
+import android.content.BroadcastReceiver
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
@@ -12,11 +15,20 @@ import com.devchernikova.firstapp.view.fragments.*
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var receiver: BroadcastReceiver
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        receiver = ConnectionChecker()
+
+        val filters = IntentFilter().apply {
+            addAction(Intent.ACTION_POWER_CONNECTED)
+            addAction(Intent.ACTION_BATTERY_LOW)
+        }
+        registerReceiver(receiver, filters)
 
         initNavigation()
 
@@ -95,6 +107,10 @@ class MainActivity : AppCompatActivity() {
             .replace(R.id.fragment_placeholder, fragment, tag)
             .addToBackStack(null)
             .commit()
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(receiver)
     }
 }
 
