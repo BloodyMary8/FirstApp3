77dcc60880d4d9b5ddba37b16edf929920d41d7a
BloodyMary8 <masharabota1996@gmail.com>
2022-10-31 17:28:06 +0700

    NOTIFICATION

--- app/src/main/java/com/devchernikova/firstapp/App.kt
+++ app/src/main/java/com/devchernikova/firstapp/App.kt
@@ -1,10 +1,14 @@
 package com.devchernikova.firstapp
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import com.devchernikova.firstapp.di.AppComponent
 import com.devchernikova.firstapp.di.DaggerAppComponent
 import com.devchernikova.firstapp.di.modules.DatabaseModule
 import com.devchernikova.firstapp.di.modules.DomainModule
+import com.devchernikova.firstapp.view.notifications.NotificationConstants.CHANNEL_ID
 import com.devchernikova.remote_module.DaggerRemoteComponent
 
 
@@ -21,6 +25,22 @@ class App : Application() {
             .databaseModule(DatabaseModule())
             .domainModule(DomainModule(this))
             .build()
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            //Задаем имя, описание и важность канала
+            val name = "WatchLaterChannel"
+            val descriptionText = "FilmsSearch notification Channel"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            //Создаем канал, передав в параметры его ID(строка), имя(строка), важность(константа)
+            val mChannel = NotificationChannel(CHANNEL_ID, name, importance)
+            //Отдельно задаем описание
+            mChannel.description = descriptionText
+            //Получаем доступ к менеджеру нотификаций
+            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            //Регистрируем канал
+            notificationManager.createNotificationChannel(mChannel)
+        }
     }
 
     companion object {
---
+++ app/src/main/java/com/devchernikova/firstapp/view/notifications/NotificationConstants.kt
@@ -0,0 +1,5 @@
+package com.devchernikova.firstapp.view.notifications
+
+object NotificationConstants {
+    const val CHANNEL_ID = "FilmsSearchChannel"
+}
